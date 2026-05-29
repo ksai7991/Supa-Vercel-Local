d1f70368426f72ec9310a2096f667fbe8ede55ba
ksai7991 <76088923+ksai7991@users.noreply.github.com>
2026-05-29 12:54:07 +0530

    Update Employee.js

--- src/pages/Employee.js
+++ src/pages/Employee.js
@@ -6,13 +6,13 @@ export default function Employees() {
       <h1>Env Test</h1>
 
       <p>
-        <strong>REACT_APP_SUPABASE_URL:</strong>{' '}
+        <strong>NEXT_PUBLIC_SUPABASE_URL:</strong>{' '}
         {String(process.env.REACT_APP_SUPABASE_URL)}
       </p>
 
       <p>
-        <strong>REACT_APP_SUPABASE_ANON_KEY exists:</strong>{' '}
-        {String(!!process.env.REACT_APP_SUPABASE_ANON_KEY)}
+        <strong>NEXT_PUBLIC_SUPABASE_ANON_KEY exists:</strong>{' '}
+        {String(!!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)}
       </p>
 
       <h2>All Environment Variables</h2>
